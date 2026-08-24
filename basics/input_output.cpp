f5b7843faae987b8a154144fc00159e9dfa906d6
kruti <sakariyakruti@gmail.com>
2026-08-24 16:46:31 +0530

    input_output.cpp
    Fix input-output program to correctly display age.

---
+++ basics/input_output.cpp
@@ -0,0 +1,13 @@
+//input-output prog
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout<<"Enter your age::" <<age <<endl;
+  cout<<"Your age is:" age;
+
+  return 0;
+}
